c2b2e371dd8b5e60e7b3f826656f429371e3cf47
Kaua <74868616+KCR781@users.noreply.github.com>
2024-12-06 15:47:00 -0300

    Projeto feito

---
+++ src/main/java/tech/buildrun/Snowman/Snowman.java
@@ -0,0 +1,12 @@
+package tech.buildrun.Snowman;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Snowman {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Snowman.class, args);
+	}
+
+}
---
+++ src/test/java/tech/buildrun/Snowman/SnowmanTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.Snowman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowmanTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
