6ef2bc78e4574cc39661a0515e6db8825a9db166
Kaua <kaua.kcr7@gmail.com>
2024-12-09 18:34:44 -0300

    Adicionado o config.js e CORS

--- src/main/java/tech/buildrun/Snowman/Snowman.java
+++ src/main/java/tech/buildrun/Snowman/Snowman.java
@@ -1,6 +1,10 @@
 package tech.buildrun.Snowman;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Snowman {
@@ -9,4 +13,18 @@ public class Snowman {
 		SpringApplication.run(Snowman.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Permite requisições de http://localhost:63342 (ou sua URL frontend)
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:63342")  // URL do frontend
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
