d290275dbaa7e212ef09044af5d1a566c1ad48b3
KCR781 <74868616+KCR781@users.noreply.github.com>
2025-05-11 18:48:49 -0300

    Updated the logic

--- src/main/java/tech/buildrun/Snowman/service/UserService.java
+++ src/main/java/tech/buildrun/Snowman/service/UserService.java
@@ -1,20 +1,20 @@
 package tech.buildrun.Snowman.service;
-
-import org.springframework.stereotype.Service;
-import tech.buildrun.Snowman.controller.CreateUserDto;
-import tech.buildrun.Snowman.controller.UpdateUserDto;
-import tech.buildrun.Snowman.entity.User;
-import tech.buildrun.Snowman.repository.UserRepository;
-
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
+import tech.buildrun.Snowman.controller.CreateUserDto;
+import tech.buildrun.Snowman.controller.UpdateUserDto;
+import tech.buildrun.Snowman.entity.User;
+import tech.buildrun.Snowman.repository.UserRepository;
+
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
