0f0e075edbb0fcea1af4b84e3f55748b2000ec06
KCR781 <74868616+KCR781@users.noreply.github.com>
2025-05-11 18:10:58 -0300

    Added a @Version annotation to the User and Manager classes. for optimistic locking.

--- src/main/java/tech/buildrun/Snowman/entity/Manager.java
+++ src/main/java/tech/buildrun/Snowman/entity/Manager.java
@@ -1,4 +1,5 @@
 package tech.buildrun.Snowman.entity;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -9,44 +10,86 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
 @Entity
 @Table(name = "tb_managers")
 public class Manager {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID managerId;
+
     @Column(name = "username")
     private String username;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "password")
     private String password;
+
     @OneToMany(mappedBy = "manager")
     private List<User> users;
 
+    @Version // Add the version field for optimistic locking
+    private Long version;
+
     public Manager() {
     }
+
     public Manager(UUID managerId, String username, String email, String password) {
         this.managerId = managerId;
         this.username = username;
         this.email = email;
         this.password = password;
     }
+
     public UUID getManagerId() {
-        return managerId;}
+        return managerId;
+    }
+
     public void setManagerId(UUID managerId) {
-        this.managerId = managerId;}
+        this.managerId = managerId;
+    }
+
     public String getUsername() {
-        return username;}
+        return username;
+    }
+
     public void setUsername(String username) {
-        this.username = username;}
+        this.username = username;
+    }
+
     public String getEmail() {
-        return email;}
+        return email;
+    }
+
     public void setEmail(String email) {
-        this.email = email;}
+        this.email = email;
+    }
+
     public String getPassword() {
-        return password;}
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
     }
 }
