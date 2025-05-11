9da9ffa157a291a91fd50df0ac8a4c4a36717a2d
KCR781 <74868616+KCR781@users.noreply.github.com>
2025-05-11 18:10:58 -0300

    Added a @Version annotation to the User and Manager classes. for optimistic locking.

--- src/main/java/tech/buildrun/Snowman/entity/User.java
+++ src/main/java/tech/buildrun/Snowman/entity/User.java
@@ -1,71 +1,124 @@
 package tech.buildrun.Snowman.entity;
-import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.Instant;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
 @Entity
 @Table(name = "tb_users")
 public class User {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID userId;
+
     @Column(name = "username")
     private String username;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "password")
     private String password;
+
     @CreationTimestamp
     private Instant creationTimestamp;
+
     @UpdateTimestamp
     private Instant updateTimestamp;
+
     @ManyToOne
     @JoinColumn(name = "manager_Id")
     private Manager manager;
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    @Version // This is the important addition for optimistic locking
+    private Long version;
+
     public User() {
     }
+
     public User(UUID userId, String username, String email, String password, Instant creationTimestamp, Instant updateTimestamp) {
         this.userId = userId;
         this.username = username;
         this.email = email;
         this.password = password;
         this.creationTimestamp = creationTimestamp;
-        this.updateTimestamp = updateTimestamp;}
+        this.updateTimestamp = updateTimestamp;
+    }
+
     public UUID getUserId() {
         return userId;
     }
+
     public void setUserId(UUID userId) {
         this.userId = userId;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public Instant getCreationTimestamp() {
         return creationTimestamp;
     }
+
     public void setCreationTimestamp(Instant creationTimestamp) {
         this.creationTimestamp = creationTimestamp;
     }
+
     public Instant getUpdateTimestamp() {
         return updateTimestamp;
     }
+
     public void setUpdateTimestamp(Instant updateTimestamp) {
         this.updateTimestamp = updateTimestamp;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
     }
 }
